b618481511b9b01f0cc310055562c1fcbc668f49
Alireza hasanpour <alirezahasanpourweb19921371@gmail.com>
2025-05-12 12:02:20 +0330

    Update constantProject.js

--- src/component/projects/constantProject.js
+++ src/component/projects/constantProject.js
@@ -1,6 +1,22 @@
 export const projectItems = [
-  {
+{
     id: 1,
+    title: "Banimode Shop",
+    description:
+      "A modern and fully functional e-commerce website built with Next.js 15",
+    img: "projects/banimode_shop.png",
+    link: "https://github.com/alirezafrontend/shop-banimode",
+  },
+  {
+    id: 2,
+    title: "Admin Panel",
+    description:
+      "A modern and responsive admin dashboard designed with Material UI (MUI) and React.",
+    img: "projects/adminPanel.png",
+    link: "https://github.com/alirezafrontend/AdminPanel---MUI-",
+  },
+  {
+    id: 3,
     title: "Weather App",
     description:
       "Clean UI Weather App developed in JavaScript",
@@ -8,7 +24,7 @@ export const projectItems = [
     link: "https://alirezafrontend.github.io/Weather-App/",
   },
   {
-    id: 2,
+    id: 4,
     title: "Parallax",
     description:
       "Modern Parallax Landing Page built with Vanilla JS",
@@ -16,7 +32,7 @@ export const projectItems = [
     link: "https://alirezafrontend.github.io/parallax.js/",
   },
   {
-    id: 3,
+    id: 5,
     title: "music player",
     description:
       "Developed an interactive music player with JavaScript.",
@@ -24,7 +40,7 @@ export const projectItems = [
     link: "https://alirezafrontend.github.io/music-player/",
   },
   {
-    id: 4,
+    id: 6,
     title: "Form Validator",
     description:
       "Developed a JavaScript form validator for user input validation",
@@ -32,7 +48,7 @@ export const projectItems = [
     link: "https://alirezafrontend.github.io/Form-Validator/",
   },
   {
-    id: 5,
+    id: 7,
     title: "Quiz App",
     description:
       "Created a dynamic quiz app in React for engaging user experiences.",
@@ -40,7 +56,7 @@ export const projectItems = [
     link: "https://quiz-app-react-x9ol.vercel.app/",
   },
   {
-    id: 6,
+    id: 8,
     title: "Credit-Card",
     description:
       "Built a dynamic credit card form with JavaScript validation.",
@@ -48,7 +64,7 @@ export const projectItems = [
     link: "https://alirezafrontend.github.io/Credit-Card-Checkout/",
   },
   {
-    id: 7,
+    id: 9,
     title: "To-Do List",
     description:
       "Built a task management app using React.",
@@ -56,7 +72,7 @@ export const projectItems = [
     link: "https://to-do-list-react-tan-seven.vercel.app/",
   },
   {
-    id: 8,
+    id: 10,
     title: "Tic-Tac-Toe",
     description:
       "Developed a dynamic Tic-Tac-Toe game with JavaScript.",
@@ -64,7 +80,7 @@ export const projectItems = [
     link: "https://alirezafrontend.github.io/Tic-Tac-Toe/",
   },
   {
-    id: 9,
+    id: 11,
     title: "Filimo",
     description:
       "Built a responsive Filimo clone using Tailwind CSS.",
